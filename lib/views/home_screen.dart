1124399cfd6abe7a33d5d56173abe1ea51791514
Wael <12910823+WaelDev@users.noreply.github.com>
2022-01-13 08:15:15 +0100

    init app

---
+++ lib/views/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff1eee5),
+      body: Container(
+        child: Center(child: Text('home')),
+      ),
+    );
+  }
+}
